7611d096fc7bd7275b66344c6b24a343eb5db341
zsxlti <57492122+zsxlti@users.noreply.github.com>
2020-03-20 17:04:03 +0100

    hamburger proba

--- WebUI/src/pages/header/header.tsx
+++ WebUI/src/pages/header/header.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { createStyles, Theme, withStyles, WithStyles } from "@material-ui/core";
+import { createStyles, Theme, withStyles, WithStyles, MuiThemeProvider } from "@material-ui/core";
 import withRoot from "./../../withRoot";
 import "typeface-roboto";
 import AppBar from "@material-ui/core/AppBar";
@@ -18,7 +18,8 @@ import { AppStore } from "./../../lib/appStore";
 import Badge from "@material-ui/core/Badge";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import { isAdmin } from "./../../services/client/roleService";
-import { BrowserView, MobileView, isMobile } from "react-device-detect";
+import { BrowserView, MobileView, isMobile, isMobileOnly, isTablet } from "react-device-detect";
+import HamburgerMenu from "react-hamburger-menu";
 
 
 const styles = (theme: Theme) =>
@@ -53,6 +54,28 @@ const styles = (theme: Theme) =>
             menuButton: {
                 marginRight: 10,
             },
+            hamburgerMenu:
+            {
+                backgroundColor: theme.palette.primary.main,
+                display: "flex",
+                flexGrow: 1,
+                alignItems: "center",
+                justifyContent: "flex-end",
+                marginRight: 10
+            },
+            mobileHeader:
+            {
+                backgroundColor: theme.palette.primary.main,
+                display: "flex",
+                flexGrow: 1,
+            },
+            mobileHeaderCart:
+            {
+                backgroundColor: theme.palette.primary.main,
+                display: "flex",
+                flexGrow: 1,
+                justifyContent:"flex-end"
+            }
         });
 
 const StyledBadge = withStyles((theme: Theme) =>
@@ -73,10 +96,14 @@ interface IState {
     isLoggedIn: boolean;
     loginStateText: string;
     cartCount: number;
+    open: boolean;
+    isOpen:boolean;
 }
 const baseUrl = "localhost:7777";
 
-interface IProps { }
+interface IProps {
+
+}
 
 class Header extends Connected<typeof React.Component, IProps & WithStyles<typeof styles> & RouteComponentProps<{}>, IState, AppStore>(React.Component)
 {
@@ -86,7 +113,9 @@ class Header extends Connected<typeof React.Component, IProps & WithStyles<typeo
         this.state = {
             isLoggedIn: true,
             loginStateText: "Kijelentkezés",
-            cartCount: this.store.state.cart.content().length
+            cartCount: this.store.state.cart.content().length,
+            open: false,
+            isOpen: false,
         }
         this.store.state.cart.count$.subscribe((data) => {
             this.setState
@@ -120,30 +149,69 @@ class Header extends Connected<typeof React.Component, IProps & WithStyles<typeo
         }
     }
 
+    handleClick = () =>
+        this.setState({
+            open: !this.state.open
+        })
+
     render() {
         const css = this.props.classes;
         const cartCount = this.state.cartCount;
-    
-        const Body = () =>
-        <div className={css.container}>
-        <AppBar position="static" className={css.appbar}>
-            <Toolbar>
-            <img src={LocalImages.images("./stikker_menu.png")} className={css.logoContainer} onClick={this.stickerClickHandler} />
-                <Typography variant="h6" onClick={this.stickerClickHandler} className={css.title}>
-                    Matricák
-                </Typography>
-               {isAdmin() && <Typography variant="h6" onClick={this.adminClickHandler} className={css.title}>
-                    Admin
-                </Typography>}
-                <IconButton aria-label="cart" className={css.right}>
-                    <StyledBadge badgeContent={cartCount} onClick={this.cartClickHandler} color="secondary">
-                        <ShoppingCartIcon />
-                    </StyledBadge>
-                </IconButton>
-                <Button className={css.button} onClick={this.logoutClickHandler}>{this.state.loginStateText}</Button>
-            </Toolbar>
-        </AppBar>
-    </div>
+
+
+        const Body = () => {
+            if (isMobileOnly) {
+                return <React.Fragment>
+                    <div className={css.mobileHeader}>
+                        <img src={LocalImages.images("./stikker_menu.png")} className={css.logoContainer} onClick={this.stickerClickHandler} />
+                        <IconButton aria-label="cart" className={css.mobileHeaderCart}>
+                            <StyledBadge badgeContent={cartCount} onClick={this.cartClickHandler} color="secondary">
+                                <ShoppingCartIcon />
+                            </StyledBadge>
+                        </IconButton>
+                        <div className={css.hamburgerMenu}>
+                            <HamburgerMenu
+                                isOpen={this.state.open}
+                                menuClicked={this.handleClick.bind(this)}
+                                width={24}
+                                height={20}
+                                strokeWidth={3}
+                                rotate={0}
+                                color="#FDB927"
+                                borderRadius={0}
+                                animationDuration={0.5}
+                            />
+                        </div>
+                        <div className={(this.state.open ? 'show' : 'hidden')}>
+                            <ul>
+                                <li>Home</li>
+                                <li>About Me</li>
+                                <li>Contact</li>
+                            </ul>
+                        </div>
+                    </div>
+                </React.Fragment>
+            }
+            else {
+                return <React.Fragment><AppBar position="static" className={css.appbar}>
+                    <Toolbar>
+                        <img src={LocalImages.images("./stikker_menu.png")} className={css.logoContainer} onClick={this.stickerClickHandler} />
+                        <Typography variant="h6" onClick={this.stickerClickHandler} className={css.title}>
+                            Matricák
+                         </Typography>
+                        {isAdmin() && <Typography variant="h6" onClick={this.adminClickHandler} className={css.title}>
+                            Admin
+                         </Typography>}
+                        <IconButton aria-label="cart" className={css.right}>
+                            <StyledBadge badgeContent={cartCount} onClick={this.cartClickHandler} color="secondary">
+                                <ShoppingCartIcon />
+                            </StyledBadge>
+                        </IconButton>
+                        <Button className={css.button} onClick={this.logoutClickHandler}>{this.state.loginStateText}</Button>
+                    </Toolbar>
+                </AppBar></React.Fragment>
+            }
+        }
         return Body();
     }
 }
